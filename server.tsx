9288965e2f203cae39455c47d186a023bf5dc873
erick <erickflan10@yahoo.com>
2024-09-28 08:04:01 -0300

    first commit

---
+++ server.tsx
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { YoutubeTranscript } = require('youtube-transcript');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/transcript', async (req, res) => {
+    const { link } = req.body;
+    const videoId = new URL(link).searchParams.get('v');
+
+    if (videoId) {
+        try {
+            const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+            const transcriptText = transcript.map(entry => entry.text).join(' ');
+            res.json({ transcript: transcriptText });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    } else {
+        res.status(400).json({ error: 'ID do vídeo inválido.' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor ouvindo na porta ${port}`);
+});
